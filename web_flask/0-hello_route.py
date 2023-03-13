9d0a1130283b2023fada3527ba1094e8c063675b
Ante-237 <akumawahanye@gmail.com>
2023-03-13 21:07:34 +0200

    documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,9 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
-    """ default home route """ 
+    ''' default home route '''
     return "Hellow HBNB!"
 
 if __name__ == '__main__':
+    ''' everything starts here '''
     app.run(host="0.0.0.0", port=5000, debug=True)
