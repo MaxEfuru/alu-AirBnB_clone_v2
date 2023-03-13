c4dd00b18b083cf6ed5ceaadb6432cbe61653ce9
Ante-237 <akumawahanye@gmail.com>
2023-03-13 23:48:37 +0200

    more flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def home():
     # """ default home route """
-    return "Hellow HBNB!"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
