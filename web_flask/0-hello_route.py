22c3f6c787c060e1c0ed865b24cbb9fc9323ef11
Ante-237 <akumawahanye@gmail.com>
2023-03-13 23:46:47 +0200

    hellow flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # flask application starting
+""" flask application starting """
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route("/")
 def home():
-    ''' default home route '''
+    # """ default home route """
     return "Hellow HBNB!"
 
 
 if __name__ == '__main__':
-    ''' everything starts here '''
+    # """ everything starts here """
     app.run(host="0.0.0.0", port=5000, debug=True)
