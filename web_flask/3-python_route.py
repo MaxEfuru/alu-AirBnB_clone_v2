e549851c460d6b52006f06da1af4399051c3fd09
Ante-237 <akumawahanye@gmail.com>
2023-03-14 11:09:51 +0200

    test fix sure

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# working with flask
-""" moving to end points  """
+# working with flask, default routes
+""" working with default routes """
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
