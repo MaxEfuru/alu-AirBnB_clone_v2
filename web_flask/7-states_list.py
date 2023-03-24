71034250407a4adb50b22a8a56411b53f2b69f4f
Ante-237 <akumawahanye@gmail.com>
2023-03-24 23:01:05 +0200

    name changed to string

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,7 +10,7 @@ app.url_map.strict_slashes = False
 
 @app.route('/states_list')
 def states_list():
-    states = storage.all(State).values()
+    states = storage.all('State').values()
     # sorted_states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=sorted_states)
 
@@ -21,4 +21,4 @@ def teardown_appcontext(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
