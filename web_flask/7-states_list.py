f2f877ead0e245b19035fa949f70e80eb260fc9d
Ante-237 <akumawahanye@gmail.com>
2023-03-24 23:12:08 +0200

    little change to variable name

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app.url_map.strict_slashes = False
 def states_list():
     states = storage.all('State').values()
     # sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
